820f6d11b2606d4b65c7ea38249944f5a6e66196
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-02 00:40:58 +0700

    fix(main): fix bugs adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import TodoView from './modules/Todo/views/TodoView/TodoView';
 import './App.scss';
 import store from './modules/Notifications/store';
 import NotificationsView from './modules/Notifications/views/NotificationsView/NotificationsView';
 const App = observer(() => {
+  const [vh, setVh] = useState(window.innerHeight * 0.01);
+  useEffect(() => {
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      setVh(window.innerHeight * 0.01);
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, [vh]);
   return (
     <>
       <div className="background"></div>
