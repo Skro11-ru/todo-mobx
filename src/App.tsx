74c1fea4184344a9ed882bc786abcf2275fd7825
Дмитрий Бунецкий <bunetskydn@gmail.com>
2022-08-01 13:32:11 +0700

    feat(main): add styles and markup for base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
-const App = () => <>Hello world!</>;
-
+import TodoView from './modules/Todo/views/TodoView/TodoView';
+import './App.scss';
+const App = () => {
+  return (
+    <>
+      <div className="background"></div>
+      <TodoView />
+    </>
+  );
+};
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,6 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
